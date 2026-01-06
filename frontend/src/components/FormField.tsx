26712fa6b594fbb44c8450f0afe2ecd174dd8061
Malalatiana Sarah Ravelonarivo <mravelon@c1r10s15.42antananarivo.mg>
2026-01-06 15:21:24 +0300

    profile ok

--- frontend/src/components/FormField.tsx
+++ frontend/src/components/FormField.tsx
@@ -12,4 +12,17 @@ const FormField : React.FC<FormFieldProps> = ({type, value}: FormFieldProps) => 
     );
 }
 
+const FormIconField : React.FC<FormFieldProps> = ({type, value}: FormFieldProps) => {
+    return (
+        <div className="flex flex-row items-center space-x-5">
+            {(value === "USER" || value === "MODERATOR")
+                ? <div className="font-icon text-4xl text-yellow-300">{type}</div>
+                : <div className="font-icon text-4xl ">{type}</div>
+            }
+            <span className="ml-2">{value}</span>
+        </div>
+    );
+}
+
+export {FormIconField};
 export default FormField;
